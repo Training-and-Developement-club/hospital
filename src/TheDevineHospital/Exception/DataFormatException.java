748300544a604d8e7d91a42be7d21418d56a373e
kotbezzabot <33574861+kotbezzabot@users.noreply.github.com>
2018-01-20 15:04:58 +0300

    20.01.2k18

---
+++ src/TheDevineHospital/Exception/DataFormatException.java
@@ -0,0 +1,7 @@
+package TheDevineHospital.Exception;
+
+public class DataFormatException extends Exception {
+    public DataFormatException(String message) {
+        super(message);
+    }
+}
--- src/TheDevineHospital/TheCommandCenterOfThisProgramm/ControlCenter.java
+++ src/TheDevineHospital/TheCommandCenterOfThisProgramm/ControlCenter.java
@@ -2,11 +2,12 @@ package TheDevineHospital.TheCommandCenterOfThisProgramm;
 
 import TheDevineHospital.DownloadFiles.URLDownload;
 import TheDevineHospital.EntityClasses.Hospital;
+import TheDevineHospital.EntityClasses.Patients.Gender;
+import TheDevineHospital.EntityClasses.Patients.Patient;
 import TheDevineHospital.EntityClasses.Patients.PatientList;
 import TheDevineHospital.Exception.MissingFile;
 import TheDevineHospital.Exception.MissingObject;
 import TheDevineHospital.InputText.HelpInput;
-import TheDevineHospital.ParseFile.ConvertToXmlFromXml.XmlConverter;
 import TheDevineHospital.ParseFile.HospitalParser.DOM;
 import TheDevineHospital.ParseFile.HospitalParser.Jackson;
 import TheDevineHospital.SearchPackage.SearchDoctorsByDate;
@@ -39,20 +40,20 @@ public class ControlCenter {
         cc.manualPrepareForWork();
 
         //***********************ConvertToXml***************************
-        /*PatientList patientList = PatientList.getInstance();
+        PatientList patientList = PatientList.getInstance();
         patientList.getPatients().add(new Patient(patientList.getPatients().size() + 1, "Андрей", "Андреев", "Петоченко",
                 "Болит колено", Gender.M,
                 HelpInput.inputDate(), "Ушиб колено"));
         patientList.getPatients().add(new Patient(patientList.getPatients().size() + 1, "Лиана", "Игоревна", "Старевич",
                 "Болит голень", Gender.F,
-                HelpInput.inputDate(), "Открытый перелом голени"));*/
-        XmlConverter.convertToXml();
+                HelpInput.inputDate(), "Открытый перелом голени"));
+        //XmlConverter.convertToXml();
 
         cc.begginingOfWork();
 
     }
 
-    public void automaticPrepareForWork() {
+    private void automaticPrepareForWork() {
         preparationForWork = PreparationForWork.newInstance();
         preparationForWork.downloadAndParsingHospital();
         URLDownload.removeURLDownload();
@@ -61,7 +62,7 @@ public class ControlCenter {
 
     }
 
-    public void manualPrepareForWork() {
+    private void manualPrepareForWork() {
         System.out.println("Ваши первые действия: " + "\n" +
                 "1)Загрузить Xml больничку" + "\n" +
                 "2)Загрузить Json больничку" + "\n" +
@@ -87,16 +88,18 @@ public class ControlCenter {
 
     }
 
-    public void functionalManualPrapareForWork(int input) throws MissingFile, MissingObject {
+    private void functionalManualPrapareForWork(int input) throws MissingFile, MissingObject {
         switch (input) {
             case 1:
                 URLDownload.getUrlDownload().downloadXml(URLDownload.LINK);
                 this.manualPrepareForWork();
                 break;
+
             case 2:
                 URLDownload.getUrlDownload().downloadJson(URLDownload.LINK1);
                 this.manualPrepareForWork();
                 break;
+
             case 3:
                 if (new File(URLDownload.getHospitalXML()).exists()) {
                     hospital = new DOM().parse(URLDownload.getHospitalXML());
@@ -114,6 +117,7 @@ public class ControlCenter {
                 }
                 this.manualPrepareForWork();
                 break;
+
             case 5:
                 if (hospital == null) {
                     throw new MissingObject("Обьект больницы отсутствует, загрузите и десиреализуйте больничку xml или json");
@@ -121,80 +125,152 @@ public class ControlCenter {
 
             default:
                 System.err.println("Некорректный ввод");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 this.manualPrepareForWork();
                 break;
         }
     }
 
 
-    public void begginingOfWork() {
+    private void begginingOfWork() {
         System.out.println("Что вы хотите сделать с докторами? : " + "\n" +
                 "1)Найти доктора" + "\n" +
                 "2)Сортировать докторов по..." + "\n" +
-                "3)Показать информацию о госпитале" + "\n" +
-                "4)Найти пациента" + "\n" +
-                "5)Сортировать пациентов по..." + "\n" +
+                "3)Найти пациента" + "\n" +
+                "4)Сортировать пациентов по..." + "\n" +
+                "5)Показать информацию о госпитале" + "\n" +
                 "6)Поиграем? :D" + "\n" +
                 "7).EXIT");
-        letsGoMassage();
+        int input = 0;
+        input = HelpInput.inputNumber();
+        letsGoMassage(input);
+
     }
 
 
-    public void letsGoMassage() {
-        int input = 0;
-        input = HelpInput.inputNumber();
-
+    private void letsGoMassage(int input) {
         if (input == 1) {
-            System.out.println("Найти: " + "\n" +
+            System.out.println("Найти доктора: " + "\n" +
                     "1)По имени" + "\n" +
-                    "2)По дате рождения" + "\n" +
-                    "3)");
+                    "2)По дате рождения" + "\n");
             input = HelpInput.inputNumber();
+            searchDoctorByArgument(input);
 
-            if (input == 1) {
-                search(hospital);
-            } else if (input == 2) {
-                SearchDoctorsByDate.search(hospital.getDoctors());
-                begginingOfWork();
-            }
         } else if (input == 2) {
-            System.out.println("Произвести сортировку по: " + "\n" +
+            System.out.println("Произвести сортировку докторов по: " + "\n" +
                     "1)Имени" + "\n" +
                     "2)Фамилии" + "\n" +
                     "3)Дате рождения");
-
             input = HelpInput.inputNumber();
+            sortDoctorsByArgument(input);
 
-            if (input == 1) {
+        } else if (input == 3) {
+            System.out.println("Найти пациента: " + "\n" +
+                    "1)По имени" + "\n" +
+                    "2)По дате рождения" + "\n");
+            input = HelpInput.inputNumber();
+            searchPatientByArgument(input);
+
+        } else if (input == 4) {
+            System.out.println("Произвести сортировку пациентов по: " + "\n" +
+                    "1)Имени" + "\n" +
+                    "2)Фамилии" + "\n" +
+                    "3)Дате рождения");
+            input = HelpInput.inputNumber();
+            sortPatientByArgument(input);
+
+        } else if (input == 5) {
+            printInfoAboutHospital();
+
+        } else if (input == 6) {
+            //*********************************Ссылка на метод управления мини-игрой*********************************************
+        } else if (input == 7) {
+            System.out.println("Программа завершает работу...");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.exit(666);
+        }
+        begginingOfWork();
+    }
+
+    private void printInfoAboutHospital() {
+        System.out.println(hospital.toString());
+        System.out.println(PatientList.getInstance().toString());
+    }
+
+    private void sortPatientByArgument(int input) {
+        Collections.sort(PatientList.getInstance().getPatients(),new SortByDate());
+        
+    }
+
+
+    private void searchPatientByArgument(int input) {
+        switch (input) {
+            case 1:
+                SearchPatientByName.search();
+                break;
+            case 2:
+                SearchPatientByDate.search(PatientList.getInstance().getPatients());
+                break;
+            default:
+                System.err.println("Некорректный ввод, попробуйте снова");
+                searchPatientByArgument(HelpInput.inputNumber());
+                break;
+
+        }
+    }
+
+    private void searchDoctorByArgument(int input) {
+        switch (input) {
+            case 1:
+                search(hospital);
+                break;
+            case 2:
+                SearchDoctorsByDate.search(hospital.getDoctors());
+                break;
+            default:
+                System.err.println("Некорректный ввод, попробуйте снова");
+                searchDoctorByArgument(HelpInput.inputNumber());
+                break;
+        }
+
+    }
+
+
+    private void sortDoctorsByArgument(int input) {
+        switch (input) {
+            case 1:
                 Collections.sort(hospital.getDoctors(), new SortByName());
                 System.out.println("Результат сортировки:");
                 System.out.println(hospital.getDoctors().toString());
-            } else if (input == 2) {
+                break;
+            case 2:
                 Collections.sort(hospital.getDoctors(), new SortBySurname());
                 System.out.println("Результат сортировки:");
                 System.out.println(hospital.getDoctors().toString());
-            } else if (input == 3) {
+                break;
+            case 3:
                 Collections.sort(hospital.getDoctors(), new SortByDate());
                 System.out.println("Результат сортировки:");
                 System.out.println(hospital.getDoctors().toString());
-            } else begginingOfWork();
-        } else if (input == 3) {
-            System.out.println(hospital.toString());
-            begginingOfWork();
-        } else if (input == 4) {
-            System.out.println("Найти: " + "\n" +
-                    "1)По имени" + "\n" +
-                    "2)По дате рождения" + "\n" +
-                    "3)");
-
-            input = HelpInput.inputNumber();
-
-            if (input == 1) {
-                SearchPatientByName.search();
-            } else if (input == 2) {
-                SearchPatientByDate.search(PatientList.getInstance().getPatients());
-            }
-        } else begginingOfWork();
+                break;
+            default:
+                System.err.println("Некорректный ввод, попробуйте снова");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                this.letsGoMassage(2);
+                break;
+        }
     }
 
 
