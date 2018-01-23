806e221d72b8963c2f780f599232a0f03191454e
kotbezzabot <vladviva5991@gmail.com>
2018-01-23 19:15:13 +0300

    23.01.2018 Factory Method

---
+++ src/TheDevineHospital/DownloadFiles/DownloadInterfaceCreator.java
@@ -0,0 +1,20 @@
+package TheDevineHospital.DownloadFiles;
+
+public interface DownloadInterfaceCreator {
+    Downloader createUrlDownloader();
+
+}
+class XmlDownloadCreator implements DownloadInterfaceCreator{
+
+    @Override
+    public Downloader createUrlDownloader() {
+        return new XmlDownloader();
+    }
+}
+class JsonDownloadCreator implements DownloadInterfaceCreator{
+
+    @Override
+    public Downloader createUrlDownloader() {
+        return new JsonDownloader();
+    }
+}
---
+++ src/TheDevineHospital/DownloadFiles/Downloader.java
@@ -0,0 +1,42 @@
+package TheDevineHospital.DownloadFiles;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public interface Downloader {
+    void urlDownload(String url);
+
+}
+
+class XmlDownloader implements Downloader {
+    private String hospitalXML = "hospital.xml";
+    @Override
+    public void urlDownload(String xmlUrl) {
+
+        try {
+            URL url = new URL(xmlUrl);
+            FileUtils.copyURLToFile(url, new File(hospitalXML));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("xml-загружен.");
+    }
+}
+
+class JsonDownloader implements Downloader {
+    private String hospitalJSON = "hospital.json";
+
+    @Override
+    public void urlDownload(String jsonUrl) {
+        try {
+            URL url = new URL(jsonUrl);
+            FileUtils.copyURLToFile(url, new File(hospitalJSON));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("json-загружен.");
+    }
+}
